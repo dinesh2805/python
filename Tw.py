47ae8010875d2eafbde27a2535ba3e1da5cfe03b
dinesh2805 <50877726+dinesh2805@users.noreply.github.com>
2019-07-22 14:25:45 +0530

    Create Tw.py

---
+++ Tw.py
@@ -0,0 +1,6 @@
+from itertools import combinations
+starrr,uti=map(int,input().split())
+lll1=len(str(starrr))
+lll2=list(combinations(str(starrry),lll1-uti))
+lll2=sorted(lll2)
+print(*lll2[0],sep='')
